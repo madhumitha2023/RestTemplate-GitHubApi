f2d62b89eb6c830cdd6eac6d4dedc24fd894c055
madhumitha2023 <madhumitha.srivatsa@gmail.com>
2024-11-08 21:48:30 -0500

    Random Post Method

--- src/main/java/co/pragra/learning/resttemplatedemo/controller/GitController.java
+++ src/main/java/co/pragra/learning/resttemplatedemo/controller/GitController.java
@@ -1,11 +1,19 @@
 package co.pragra.learning.resttemplatedemo.controller;
 
 import co.pragra.learning.resttemplatedemo.dto.GitHubUser;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,4 +26,30 @@ public class GitController {
         System.out.println("gitUser : " + gitUser);
         return gitUser;
     }
+
+    @PostMapping("/users")
+    public ResponseEntity<String> postRandom(){
+        Request request = new Request();
+        request.setFirstName("Testing");
+        request.setLastName("Learning");
+        request.setEmail("testing@pragra.io");
+        request.setSubject("Java FS");
+        request.setQuery("All good at our end!");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+        HttpEntity<Request> requestEntity = new HttpEntity<>(request, headers);
+        ResponseEntity<String> mapResponseEntity = restTemplate.postForEntity("https://app.pragra.io/api/engagement/contact", requestEntity, String.class);
+        System.out.println("mapResponseEntity.getBody() : " + mapResponseEntity.getBody());
+        return mapResponseEntity;
+    }
+}
+
+@Data
+class Request{
+   private String firstName;
+   private String lastName;
+    private String email;
+    private String subject;
+    private String query;
 }
